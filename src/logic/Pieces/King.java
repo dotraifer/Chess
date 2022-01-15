398e0cb82b247e87385e1322f7455f1873426f06
Dotan Raifer <dotanraifer@gmail.com>
2022-01-15 03:00:30 +0200

    15.1.22 end of day. pieces finish

--- src/logic/Pieces/King.java
+++ src/logic/Pieces/King.java
@@ -4,15 +4,34 @@ import logic.Board;
 import logic.Color;
 import logic.Move;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class King extends Piece {
+    final int[] move_mask = {-9, -8, -7, -1, 1, 7, 8, 9};
     public King(int position, Color color) {
         super(position, color);
     }
 
     @Override
     public List<Move> getLegalMoves(Board board) {
-        return null;
+        int possible_coordinate;
+        List<Move> legalMoves = new ArrayList<>();
+        for (int mask : move_mask) {
+            if (isFirstColumnExtremeCase(position, mask))
+                continue;
+            possible_coordinate = mask + position;
+            if (isValidCoordinate(possible_coordinate))
+                if (!isFriendlyPieceOnCoordinate(board, possible_coordinate)) {
+                    legalMoves.add(new Move(board, this, possible_coordinate));
+                }
+        }
+        return legalMoves;
+
+    }
+    public boolean isFirstColumnExtremeCase(int coordinate, int mask) {
+        if (coordinate % 8 == 0 && (mask == -1 || mask == -9 || mask == 7))
+            return true;
+        else return ((coordinate + 1) % 8 == 0 && (mask == 1 || mask == -7 || mask == 9));
     }
 }
--- src/logic/Pieces/Pawn.java
+++ src/logic/Pieces/Pawn.java
@@ -8,10 +8,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pawn extends Piece {
-    final int[] move_mask = {8};
+    final int[] move_mask = {8, 16};
+    private boolean isFirstMove;
 
     public Pawn(int position, Color color) {
         super(position, color);
+        this.isFirstMove = true;
     }
 
     @Override
@@ -19,10 +21,28 @@ public class Pawn extends Piece {
         int possible_coordinate;
         List<Move> legalMoves = new ArrayList<>();
         for (int mask : move_mask) {
-            possible_coordinate = mask + (position * getDirection(color));
+            possible_coordinate = position + (mask * getDirection(color));
             if (isValidCoordinate(possible_coordinate)) {
                 if (mask == 8 && board.board_state.get(possible_coordinate) == null) {
-                    legalMoves.add(new Move(board, this, possible_coordinate * getDirection(color)));
+                    legalMoves.add(new Move(board, this, possible_coordinate));
+                }
+                else if (mask == 16 && board.board_state.get(possible_coordinate) == null && isFirstMove)
+                {
+                    if(board.board_state.get(possible_coordinate + 8 * getDirection(color)) == null)
+                    {
+                        legalMoves.add(new Move(board, this, possible_coordinate));
+                        isFirstMove = false;
+                    }
+                }
+                if(isFirstColumnExtremeCase(possible_coordinate, mask, color))
+                    break;
+                else if(mask == 7 && board.board_state.get(possible_coordinate) != null && !isFriendlyPieceOnCoordinate(board, possible_coordinate))
+                {
+                    legalMoves.add(new Move(board, this, possible_coordinate));
+                }
+                else if(mask == 9 && board.board_state.get(possible_coordinate) != null && !isFriendlyPieceOnCoordinate(board, possible_coordinate))
+                {
+                    legalMoves.add(new Move(board, this, possible_coordinate));
                 }
             }
         }
@@ -33,5 +53,11 @@ public class Pawn extends Piece {
         if (color == Color.White)
             return -1;
         return 1;
+    }
+    public boolean isFirstColumnExtremeCase(int coordinate, int mask, Color color) {
+        // TODO possible problem with direction
+        if (coordinate % 8 == 0 && ((mask == 7 && color == Color.Black)||(mask == 9 && color == Color.White)))
+            return true;
+        else return ((coordinate + 1) % 8 == 0 && ((mask == 9 && color == Color.Black)||(mask == 7 && color == Color.White)));
     }
 }
