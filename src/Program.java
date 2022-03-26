d671ce4cc71fb2fcc87eb9c82c0b30c66c03319e
Dotan Raifer <dotanraifer@gmail.com>
2022-03-26 22:38:48 +0300

    board and pieces gui added

--- src/Program.java
+++ src/Program.java
@@ -22,6 +22,8 @@ public class Program {
         {
             System.out.println(move.toString());
         }
+
+
         GameScreen gameScreen = new GameScreen();
 
     }
--- src/gui/GameScreen.java
+++ src/gui/GameScreen.java
@@ -1,13 +1,23 @@
 package gui;
 
+import logic.Board;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class GameScreen {
-    private final JFrame frame;
+    private final JFrame gameFrame;
+    private final BoardPanel boardPanel;
+
     public GameScreen()
     {
-        this.frame = new JFrame("Chess");//creating instance of JFrame
-        this.frame.setSize(400, 400);
-        this.frame.setVisible(true);
+        this.gameFrame = new JFrame("Chess");//creating instance of JFrame
+        this.gameFrame.setLayout(new BorderLayout());
+        this.gameFrame.setSize(400, 400);
+
+        this.boardPanel = new BoardPanel();
+        this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
+
+        this.gameFrame.setVisible(true);
     }
 }
